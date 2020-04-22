4508d29d290529743e71b79ef259914e31529ea2
Asbjørn Kallevik <asbjornkallevik@Asbjrns-iMac.local>
2020-04-22 13:28:40 +0200

    Displaying input fields in time-adjustment section!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // MODULE
 const timeSequence = (function() {
     let details = {
-        totalHours: 0,
-        expectedMs: 0,
+        totalHours: 8,
+        expectedMs: 28800000
 
     }
 
@@ -17,6 +17,58 @@ function getTotalHours(hours) {
         toHour = new Date("01/01/2007 " + hours[1]).getHours();
 
     return ((toHour - fromHour) + 1);
+}
+// Get current hour
+function getHour(value, i) {
+    let hour = new Date("01/01/2007 " + value).getHours() + i;
+
+    if (hour < 10) {
+        hour = "0" + hour + ":00";
+    } else {
+        hour += ":00";
+    }
+    return hour;
+}
+
+// Show time adjustment section
+function showTimeAdjustment(selectedHours) {
+    const timeAdjustment = document.querySelector('.time-adjustment'),
+        timeInputs = document.querySelector('#time-inputs'),
+        hours = timeSequence.details.totalHours;
+    let i = 0;
+
+    // Show section
+    timeAdjustment.classList.add('d-block');
+
+    while (i < hours) {
+        const inputGroup = document.createElement('div'),
+            calcBtn = document.createElement('div');
+
+         // Assign classes and content to input group
+        inputGroup.classList.add('input-group');
+        inputGroup.classList.add('time-adjustment__input-group');
+       
+        inputGroup.innerHTML = `
+            <div class="input-group time-adjustment__input-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text time-adjustment__input-prepend">${getHour(selectedHours[0], i)}</span>
+                </div>
+                <input type="text" class="form-control" id="hr-${i + 1}-min" placeholder="min">
+                <input type="text" class="form-control" id="hr-${i + 1}-sec" placeholder="sek">
+                <input type="text" class="form-control" id="hr-${i + 1}-ms" placeholder="ms">
+            </div>
+        `;
+
+        // Append elements
+        timeInputs.appendChild(inputGroup);
+        if (i === (hours - 1)) {
+            calcBtn.innerHTML = '<button type="button" class="btn btn-danger btn-lg btn-block time-adjustment__calculate" id="calcBtn">Beregn</button>';
+            timeInputs.appendChild(calcBtn);
+        }
+
+        i++;
+    }
+
 }
 
 (function () {
@@ -40,9 +92,10 @@ function getTotalHours(hours) {
         console.log(timeSequence.details);
 
         // Hide time-selector section
-        // timeSelector.classList.add('d-none');
+        timeSelector.classList.add('d-none');
 
         // Show time-adjustment section
+        showTimeAdjustment(selectedHours);
 
     });
     
