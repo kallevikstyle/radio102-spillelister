ba930e6ac7a6ec6989f0099904b231927ab7fe21
Asbjørn Kallevik <asbjornkallevik@Asbjrns-iMac.local>
2020-04-23 00:35:04 +0200

    Adding calculations to script, and displaying results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 // MODULE
 const timeSequence = (function() {
     let details = {
-        totalHours: 8,
-        expectedMs: 28800000
+        totalHours: 0,
+        expectedMs: 0
 
     }
 
@@ -29,6 +29,65 @@ function getHour(value, i) {
     }
     return hour;
 }
+// Convert milliseconds to minutes and seconds
+function millisToMinutesAndSeconds(millis) {
+    var ms = Math.abs(millis);
+    var minutes = Math.floor(ms / 60000);
+    var seconds = ((ms % 60000) / 1000).toFixed(0);
+    return minutes + " minutter og " + (seconds < 10 ? '0' : '') + seconds + " sekunder";
+  }
+
+// Calculate milliseconds
+function getMilliseconds(minutes, seconds, milliseconds) {
+    let sum = 0;
+
+    // Minutes
+    for (let i = 0; i < minutes.length; i++) {
+        sum += (minutes[i].value * 60000);
+    }
+    // Seconds
+    for (let i = 0; i < seconds.length; i++) {
+        sum += (seconds[i].value * 1000);
+    }
+    // Milliseconds
+    for (let i = 0; i < milliseconds.length; i++) {
+        sum += (milliseconds[i].value * 1);
+    }
+    
+    return sum;
+}
+
+// Calculate button events
+function calculateHours() {
+    const calcBtn = document.querySelector('#calcBtn');
+
+    calcBtn.addEventListener('click', function() {
+        const inputMin = document.querySelectorAll('.input-min'),
+            inputSec = document.querySelectorAll('.input-sec'),
+            inputMs = document.querySelectorAll('.input-ms'),
+            currentDuration = document.querySelector('#currentDuration'),
+            durationDiff = document.querySelector('#durationDiff'),
+            sumMs = getMilliseconds(inputMin, inputSec, inputMs),
+            difference = sumMs - timeSequence.details.expectedMs;
+        let sumComment = "";
+        console.log(sumMs);
+
+        if (difference > 0) {
+            sumComment = " over."
+        } else if (difference < 0) {
+            sumComment = " under."
+        } else {
+            sumComment = ".";
+        }
+        
+        // Display current duration in result table
+        currentDuration.innerHTML = `${sumMs} millisekunder`;
+        // Display difference
+        durationDiff.innerHTML = `${difference} millisekunder<br>
+        (${millisToMinutesAndSeconds(difference)}${sumComment})`;
+    });
+
+}
 
 // Show time adjustment section
 function showTimeAdjustment(selectedHours) {
@@ -53,22 +112,22 @@ function showTimeAdjustment(selectedHours) {
                 <div class="input-group-prepend">
                     <span class="input-group-text time-adjustment__input-prepend">${getHour(selectedHours[0], i)}</span>
                 </div>
-                <input type="text" class="form-control" id="hr-${i + 1}-min" placeholder="min">
-                <input type="text" class="form-control" id="hr-${i + 1}-sec" placeholder="sek">
-                <input type="text" class="form-control" id="hr-${i + 1}-ms" placeholder="ms">
+                <input type="text" class="form-control input-min" id="hr${i + 1}-min" placeholder="min">
+                <input type="text" class="form-control input-sec" id="hr${i + 1}-sec" placeholder="sek">
+                <input type="text" class="form-control input-ms" id="hr${i + 1}-ms" placeholder="ms">
             </div>
         `;
 
         // Append elements
         timeInputs.appendChild(inputGroup);
+        // Append calculate button
         if (i === (hours - 1)) {
             calcBtn.innerHTML = '<button type="button" class="btn btn-danger btn-lg btn-block time-adjustment__calculate" id="calcBtn">Beregn</button>';
             timeInputs.appendChild(calcBtn);
+            calculateHours();
         }
-
         i++;
     }
-
 }
 
 (function () {
@@ -79,7 +138,8 @@ function showTimeAdjustment(selectedHours) {
         const timeSelector = document.querySelector('.time-selector'),
             fromHour = document.querySelector('#fromHour'),
             toHour = document.querySelector('#toHour'),
-            selectedHours = [fromHour.value, toHour.value];
+            selectedHours = [fromHour.value, toHour.value],
+            targetDuration = document.querySelector('#targetDuration');
         let totalHours = 0,
             totalMs = 0;
 
@@ -89,13 +149,16 @@ function showTimeAdjustment(selectedHours) {
         // Add total hours and ms to module
         timeSequence.details.totalHours = totalHours;
         timeSequence.details.expectedMs = totalMs;
-        console.log(timeSequence.details);
 
         // Hide time-selector section
         timeSelector.classList.add('d-none');
 
         // Show time-adjustment section
         showTimeAdjustment(selectedHours);
+
+        // Display target duration in result table
+        targetDuration.innerHTML = `${timeSequence.details.expectedMs} millisekunder`;
+
 
     });
     
